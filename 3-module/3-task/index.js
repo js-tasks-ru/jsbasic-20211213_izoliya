82a48cd9d50db3c9c1e20cd0ae2a97a4fcda2921
Olesya Luka <izoliya@gmail.com>
2021-12-26 22:30:15 +0500

    3 module 1-3 task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('');
+  let index = arr.indexOf('-');
+while (index != -1) {
+  arr.splice(index, 1);
+  let b = arr[index].toUpperCase();
+  arr.splice(index, 1, b);
+  index = arr.indexOf('-', index + 1);
+}
+return (arr.join(''));
 }
