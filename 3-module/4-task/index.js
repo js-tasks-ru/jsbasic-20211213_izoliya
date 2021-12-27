84f64273bdc978281691042fd5cae71294ba409f
Olesya Luka <izoliya@gmail.com>
2021-12-27 15:57:40 +0500

    3 module 4 task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
   // ваш код...
+  let newMass = [];
+  users.forEach (function(user) {
+    if (user.age <= age) {
+    newMass.push (user.name + ', ' + user.balance);
+    }
+  });
+  return ( newMass.join('\n') );
 }
